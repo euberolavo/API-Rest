657f5e32b94d1d42dc5389c46baa52a7b69095db
euberolavo <euber.olavo.bezerra@gmail.com>
2021-08-28 23:51:33 -0300

    Rotas do CRUD das tabelas Paciente, especialidade e atendimentos criadas

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,38 +2,123 @@ const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
 
+    //Cria Rota GET de Atendimentos Geral
     app.get('/atendimentos', (req, res) => {
-        Atendimento.lista(res)
+        Atendimento.listaAtendimentos(res)
     })
 
+    //Cria Rota GET de Especialidades Geral
+    app.get('/especialidades', (req, res) => {
+        Atendimento.listaEspecialidades(res)
+    })
+
+    //Cria Rota GET de Pacientes Geral
+    app.get('/pacientes', (req, res) => {
+        Atendimento.listaPacientes(res)
+    })
+
+    //Cria Rota GET de Atendimentos recebendo parametros de requisição
     app.get('/atendimentos/:id', (req, res) => {
 
         const id = parseInt(req.params.id)
 
-        Atendimento.buscaPorId(id, res)
+        Atendimento.buscaAtendimentoPorId(id, res)
     })
 
+    //Cria Rota GET de Especialidades recebendo parametros de requisição
+    app.get('/especialidades/:id', (req, res) => {
+
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaEspecialidadePorId(id, res)
+    })
+
+    //Cria Rota GET de Pacientes recebendo parametros de requisição
+    app.get('/pacientes/:id', (req, res) => {
+
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPacientePorId(id, res)
+    })
+
+    //Cria Rota POST de Atendimentos
     app.post('/atendimentos', (req, res) => {
 
         const atendimento = req.body
 
         console.log(req.body)
 
-        Atendimento.adiciona(atendimento)
-        res.send('Post Atendimento')
+        Atendimento.cadastroAtendimento(atendimento)
+        res.send('Atendimento gerado com sucesso')
     })
 
+    //Cria Rota POST de Pacientes
+    app.post('/pacientes', (req, res) => {
+
+        const paciente = req.body
+
+        console.log(req.body)
+
+        Atendimento.cadastroPaciente(paciente)
+        res.send('Paciente Cadastrado')
+    })
+
+    //Cria Rota POST de Especialidades
+    app.post('/especialidades', (req, res) => {
+
+        const especialidades = req.body
+
+        console.log(req.body)
+
+        Atendimento.cadastroEspecialidade(especialidades)
+        res.send('Especialidade Cadastrada')
+    })
+
+    // Faz alteração em Pacientes identificado pelo id
+    app.patch('/pacientes/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.alteraPaciente(id, valores, res)
+
+    })
+
+    // Faz alteração em Especialidades identificado pelo id
+    app.patch('/especialidades/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.alteraEspecialidade(id, valores, res)
+
+    })
+
+    // Faz alteração em Atendimentos identificado pelo id
     app.patch('/atendimentos/:id', (req,res) => {
         const id = parseInt(req.params.id)
         const valores = req.body
 
-        Atendimento.altera(id, valores, res)
+        Atendimento.alteraAtendimento(id, valores, res)
 
     })
 
+    // Exclui Atendimento especificado pelo id
     app.delete('/atendimentos/:id', (req,res) => {
         const id = parseInt(req.params.id)
 
-        Atendimento.deleta(id, res)
+        Atendimento.deletaAtendimento(id, res)
+    })
+
+    // Exclui Especialidade especificada pelo id
+    app.delete('/especialidades/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deletaEspecialidade(id, res)
+    })
+
+    // Exclui Paciente especificado pelo id
+    app.delete('/pacientes/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deletaPaciente(id, res)
     })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,13 @@
 const conexao = require('../infraestrutura/conexao')
 
-class Atendimento {
-    adiciona(atendimento) {
 
-        const sql = 'INSERT INTO Atendimentos(cliente, especialidade) VALUES ($1, $2)'
-        conexao.query(sql, [atendimento.cliente, atendimento.especialidade], (erro, resultados) => {
+class Atendimento {
+
+    // Gera Atendimento
+    cadastroAtendimento(atendimento) {
+
+        const sql = 'insert into atendimentos (id_paciente, data_consulta, id_especialidade) values ($1,$2,$3)'
+        conexao.query(sql, [atendimento.paciente, atendimento.dataConsulta, atendimento.especialidade], (erro, resultados) => {
 
             if (erro) {
                 console.log(erro)
@@ -15,26 +18,82 @@ class Atendimento {
         })
     }
 
-    lista(res) {
-        const sql = 'SELECT * FROM Atendimentos'
+    // Cadastra Paciente
+    cadastroPaciente(paciente) {
 
-        console.log(sql)
+        const sql = 'INSERT INTO pacientes (nome_paciente,data_nasc_paciente, cpf_paciente) VALUES ($1,$2,$3)'
+        conexao.query(sql, [paciente.nome, paciente.data_nasc, paciente.cpf], (erro, resultados) => {
+
+            if (erro) {
+                console.log(erro)
+            } else {
+                console.log(resultados)
+
+            }
+        })
+    }
+
+    // Cadastra Especialidade
+    cadastroEspecialidade(especialidade) {
+
+        const sql = 'insert into especialidades (especialidade) values ($1)'
+        conexao.query(sql, [especialidade.especialidade], (erro, resultados) => {
+
+            if (erro) {
+                console.log(erro)
+            } else {
+                console.log(resultados)
+
+            }
+        })
+    }
+
+    // Lista todos os atendimentos Cadastrados
+    listaAtendimentos(res) {
+        const sql = 'select atendimentos.id, pacientes.nome_paciente, pacientes.data_nasc_paciente, atendimentos.data_consulta, especialidades.especialidade from atendimentos INNER JOIN pacientes ON atendimentos.id_paciente = pacientes.id INNER JOIN especialidades ON atendimentos.id_especialidade = especialidades.id'
+
         conexao.query(sql, (erro, resultados) => {
             if (erro) {
                 res.status(400).json(erro)
             } else {
                 res.status(200).json(resultados.rows)
-                // console.log(resultados.rows)
             }
         })
     }
 
-    buscaPorId(id,res) {
-        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
+    // Lista todos os Especialidades Cadastradas
+    listaEspecialidades(res) {
+        const sql = 'SELECT * FROM Especialidades'
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados.rows)
+            }
+        })
+    }
+
+    // Lista todos os Pacientes Cadastradas
+    listaPacientes(res) {
+        const sql = 'SELECT * FROM Pacientes'
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados.rows)
+            }
+        })
+    }
+
+    // Lista atendimento especificado pelo id
+    buscaAtendimentoPorId(id, res) {
+        const sql = `SELECT atendimentos.id, pacientes.nome_paciente, pacientes.data_nasc_paciente, atendimentos.data_consulta, especialidades.especialidade FROM atendimentos INNER JOIN pacientes ON atendimentos.id_paciente = pacientes.id INNER JOIN especialidades ON atendimentos.id_especialidade = especialidades.id WHERE atendimentos.id = ${id}`
 
         conexao.query(sql, (erro, resultados) => {
             const atendimento = resultados.rows[0]
-            if(erro){
+            if (erro) {
                 res.status(400).json(erro)
             } else {
                 res.status(200).json(atendimento)
@@ -42,29 +101,112 @@ class Atendimento {
         })
     }
 
-    altera(id, valores, res) {
-        const sql = 'UPDATE Atendimentos SET cliente = ($1) , especialidade = ($2) WHERE id = ($3);'
+    // Lista Especialidade especifica pelo id
+    buscaEspecialidadePorId(id, res) {
+        const sql = `SELECT * FROM Especialidades WHERE id=${id}`
 
-        conexao.query(sql,[valores.cliente, valores.especialidade, id], (erro, resultados) => {
-            if(erro) {
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados.rows[0]
+            if (erro) {
                 res.status(400).json(erro)
-            }else {
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    // Lista Paciente especificado pelo id
+    buscaPacientePorId(id, res) {
+        const sql = `SELECT * FROM Pacientes WHERE id=${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados.rows[0]
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    // Altera paciente especificado pelo id
+    alteraPaciente(id, valores, res) {
+        const sql = 'UPDATE pacientes SET nome_paciente = ($1) , data_nasc_paciente = ($2), cpf_paciente = ($3)  WHERE id = ($4);'
+
+        conexao.query(sql, [valores.nome, valores.data_nasc, valores.cpf, id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
                 res.status(200).json(resultados)
             }
         })
     }
 
-    deleta(id, res) {
+    // Altera Especialidade especificado pelo id
+    alteraEspecialidade(id, valores, res) {
+        const sql = 'UPDATE especialidades SET especialidade = ($1)  WHERE id = ($2);'
+
+        conexao.query(sql, [valores.especialidade, id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    // Altera Atendimento especificado pelo id
+    alteraAtendimento(id, valores, res) {
+        const sql = 'UPDATE atendimentos SET id_paciente = ($1), data_consulta = ($2), id_especialidade = ($3)  WHERE id = ($4);'
+
+        conexao.query(sql, [valores.paciente, valores.dataConsulta, valores.especialidade, id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    // Exclui Atendimento especificado pelo id
+    deletaAtendimento(id, res) {
         const sql = 'DELETE FROM Atendimentos WHERE id=($1)'
 
         conexao.query(sql, [id], (erro, resultados) => {
-            if(erro) {
+            if (erro) {
                 res.status(400).json(erro)
                 console.log(sql)
             } else {
                 res.status(200).json(resultados)
             }
         })
-    }    
+    }
+
+    // Exclui Especialidade especificado pelo id
+    deletaEspecialidade(id, res) {
+        const sql = 'DELETE FROM Especialidades WHERE id=($1)'
+
+        conexao.query(sql, [id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    // Exclui Paciente especificado pelo id
+    deletaPaciente(id, res) {
+        const sql = 'DELETE FROM Pacientes WHERE id=($1)'
+
+        conexao.query(sql, [id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+                console.log(sql)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
 }
 module.exports = new Atendimento
